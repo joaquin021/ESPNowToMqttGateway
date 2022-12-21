0d7ea42564b5b0771979245bfe23440762cd9bf2
Joaquin Roca Soler <joaquinrs91@gmail.com>
2022-12-21 21:41:01 +0100

    add debug flag

--- lib/commons/Commons.h
+++ lib/commons/Commons.h
@@ -1,10 +1,17 @@
 #ifndef __COMMONS_H
 #define __COMMONS_H
 
+#define DEBUG_FLAG
+
+#ifdef DEBUG_FLAG
 #define debug Serial.print
 #define debugln Serial.println
 #define debugf Serial.printf
-
+#else
+#define debug
+#define debugln
+#define debugf
+#endif
 #define BAUD_RATE 115200
 #define ESPNOW_BUFFERSIZE 200
 #define END_TX_CHAR '|'
